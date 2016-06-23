90f2ae2acd1a344d2966cf3944604478f3ab8351
David Levey <cpdlevey@gmail.com>
2016-06-23 10:11:10 -0500

    stopped checking from the cache files

--- check_for_custom_vhosts.sh
+++ check_for_custom_vhosts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-custom_templates="$(find /var/cpanel/userdata -type f -name '*.cache' -exec grep -l 'custom_vhost_template' {} \;)";
+custom_templates="$(find /var/cpanel/userdata -type f ! -name '*.cache' -exec grep -l 'custom_vhost_template' {} \;)";
 
 	echo "Checking for custom vhost templates in the /usr/local/apache directory...";
 	echo "";
@@ -7,6 +7,6 @@ custom_templates="$(find /var/cpanel/userdata -type f -name '*.cache' -exec grep
 for custom_template in $(echo "$custom_templates"); do 
 	custom_temp="$(grep -l 'usr/local/apache' $custom_template)"; 
 	if [ ! -z "$custom_temp" ]; then 
-		python -mjson.tool $custom_template |grep 'custom_vhost_template'
+		grep -H custom_vhost_template $custom_template
 	fi; 
 done
